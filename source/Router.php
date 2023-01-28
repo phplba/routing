9c9eef251388ae1996ba0d9c535565d74f2f04da
Scrooge McDuck <angelisium@daum.net>
2023-01-28 14:47:32 +0100

    Create Router.php

---
+++ source/Router.php
@@ -0,0 +1,52 @@
+<?php
+namespace phplba\Routing;
+
+use phplba\Routing\Error\RoutingError;
+
+class Router {
+	public array $static;
+	public array $dynamic;
+
+	public function __construct(array $routes, public string $regex) {
+		$this->static = $routes['static'];
+		$this->dynamic = $routes['dynamic'];
+	}
+
+	public function resolve(string $URL = null) {
+		if ($URL = null) {
+			$URL = self::getPATH();
+		}
+		if (isset($this->static[$URL])) {
+			return [
+				'route' => $this->static[$URL],
+				'matches' => [],
+			];
+		}
+		if (!preg_match($this->regex, $URL, $matches)) {
+			throw new RoutingError(
+				"No route was found for this URL: $URL",
+				404
+			);
+		}
+		if (isset($this->dynamic[$matches['MARK']])) {
+			$route = $this->dynamic[$matches['MARK']];
+			unset($matches['MARK']);
+			return [
+				'route' => $route,
+				'matches' => $matches,
+			];
+		}
+		throw new RoutingError(
+			"The route was matched in the regular expression, but no matching route was found. (URL: $URL)",
+			500
+		);
+	}
+
+	public static function getPATH(): string {
+		$URI = $_SERVER['REQUEST_URI'];
+		if (false !== ($I = strpos($URI, '?'))) {
+			$URI = substr($URI, 0, $I);
+		}
+		return rawurldecode($URI);
+	}
+}
